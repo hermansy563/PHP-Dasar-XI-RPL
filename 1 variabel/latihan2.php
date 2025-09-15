4fbc5ff5121bd382a713d9440c85a41922f2481d
hermansy563 <hermansy563@gmail.com>
2025-09-15 13:34:24 +0700

    variabel
    PHP-Dasar-Variabel

---
+++ 1 variabel/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variabel latihan 2</title>
+</head>
+<body>
+    <?php
+    $nama="hermansyah";
+    $kelas="XI RPL";
+    $alamat="kp.reunghasjajar";
+
+    echo "nama : $nama";
+    echo "<br>";
+    echo "kelas : $kelas";
+    echo "<br>";
+    echo "alamat : $alamat";
+    ?>
+</body>
+</html>
